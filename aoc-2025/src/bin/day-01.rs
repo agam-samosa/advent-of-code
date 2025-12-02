2d30a1d1d4bf76bfc99bffeecf1e34ce6b1d4d34
Agam Sama <him@agamsama.com>
2025-12-02 18:51:49 +0000

    feat: solve day 1 (part 1)

--- aoc-2025/src/bin/day-01.rs
+++ aoc-2025/src/bin/day-01.rs
@@ -1,3 +1,37 @@
-fn main() {
-    todo!()
+use anyhow::bail;
+use std::io;
+
+fn main() -> anyhow::Result<()> {
+    let input = io::read_to_string(io::stdin())?;
+
+    println!("Part 1: {}", part_1(&input)?);
+
+    Ok(())
+}
+
+fn part_1(inp: &str) -> anyhow::Result<i32> {
+    let mut dial = 50;
+    let mut z_count = 0;
+
+    for line in inp.lines() {
+        let (dir, mag_raw) = line.split_at(1);
+        let mag: i32 = mag_raw.parse()?;
+
+        match dir {
+            "L" => {
+                dial -= mag;
+            }
+            "R" => {
+                dial += mag;
+            }
+            _ => bail!("Invalid rotation!"),
+        }
+        dial = dial.rem_euclid(100);
+
+        if dial == 0 {
+            z_count += 1;
+        }
+    }
+
+    Ok(z_count)
 }
